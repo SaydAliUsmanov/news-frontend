6dd0e46fe3009c8b7b8700a3742ceb573d529b85
SaydAliUsmanov <usmanov.guno@gmail.com>
2022-09-18 11:21:16 +0300

    Разбил компоненты на чанки с помощью react lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import Counter from "./components/Counter";
+import React, { Suspense } from 'react';
 import "./index.scss";
+import {MainPageLazy} from "./Pages/MainPage/MainPage.lazy";
+import {AboutPageLazy} from "./Pages/AboutPage/AboutPage.lazy";
+import {Link, Route, Routes} from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <Counter/>
-    </>
+    <div>
+      <Link to={'/'} >Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/'} element={<MainPageLazy /> }/>
+          <Route path={'/about'} element={<AboutPageLazy />}/>
+        </Routes>
+      </Suspense>
+    </div>
   );
 };
 
---
+++ src/Pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      О сайте!
+    </div>
+  );
+};
+
+export default AboutPage;
